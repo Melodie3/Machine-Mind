ef25f5b603b124a52245dfcdf92043f615129591
MrSquirrelDeDuck <72826048+MrSquirrelDeDuck@users.noreply.github.com>
2024-11-24 16:27:54 -0500

    Change the equation for corrupted bread.

--- bread/space.py
+++ bread/space.py
@@ -1028,16 +1028,32 @@ def get_corruption_chance(
         xpos: int,
         ypos: int,
     ) -> float:
-    """Returns the chance of a loaf becoming corrupted for any point in the galaxy. Between 0 and 1."""
+    """Returns the chance of a loaf becoming corrupted for any point in the galaxy. Between 0 and 1.
+    
+    Subtract the map radius from a regular coordinate for this."""
     dist = math.hypot(xpos, ypos)
 
-    # The band where the chance is 0 is between ~80 and ~87.774.
-    if 80 <= dist <= 87.774:
+    # The band where the chance is 0 is between 80 and 87.
+    if 80 <= dist <= 87:
         return 0.0
+    
+    # f\left(x\right)=\left\{x\le2:99,2\le x\le80:\left(\frac{\cos\left(\left(x-2\right)\frac{\pi}{78}\right)}{2}+0.5\right)99,80<x<87:0,87\le x\le241.81799:\left(\frac{\cos\left(\frac{\left(x-87\right)\pi}{154.8179858682464038403596020291203352621852869144970764695290884}\right)}{-2}+0.5\right)99,x>241.81799:99\right\}
+    # Where `x` is the distance to (0, 0)
+    # lmao
 
-    # `\frac{d^{2.15703654359}}{1000}+118e^{-0.232232152965\left(\frac{d}{22.5}\right)^{2}}-19`
-    # where `d` is the distance to (0, 0).
-    return ((dist ** 2.15703654359 / 1000) + 118 * math.e ** (-0.232232152965 * (dist / 22.5) ** 2) - 19) / 100
+    # It's a piecewise function, so `if` statements are used.
+    if dist <= 2:
+        chance = 99
+    elif dist <= 80:
+        chance = (math.cos((dist - 2) * (math.pi / 78)) / 2 + 0.5) * 99
+    elif dist <= 87:
+        chance = 0
+    elif dist <= 241.81799:
+        chance = (math.cos(((dist - 87) * math.pi) / 154.8179858682464038403596020291203352621852869144970764695290884) / -2 + 0.5) * 99
+    else:
+        chance = 99
+
+    return chance / 100
 
 def get_anarchy_corruption_chance(
         xpos: int,
